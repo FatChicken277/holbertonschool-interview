be056ee59b92f8a68c868a79cd176cd1d67db1d9
FatChicken277 <1637@holbertonschool.com>
2020-12-17 18:56:37 -0500

    add conditional

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -12,6 +12,9 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new, *aux;
 
+	if (head != NULL)
+		return NULL;
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
