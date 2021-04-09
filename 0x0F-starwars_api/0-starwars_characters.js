0cc8c17da066033c409a9d58cfcca5d78958840e
FatChicken277 <aramirez@truora.com>
2021-04-08 23:25:03 -0500

    use async function

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,17 +1,31 @@
 #!/usr/bin/node
+
+// 1. Create a new function that returns a promise
+function firstFunction (char) {
+  const request = require('request');
+  return new Promise((resolve, reject) => {
+    request(char, function (err, res, body) {
+      if (res.statusCode === 200) {
+        resolve(JSON.parse(body).name);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+// 2. Create an async function
+async function secondFunction (body) {
+  for (const char of JSON.parse(body).characters) {
+    console.log(await firstFunction(char));
+  }
+}
+
 if (process.argv.length === 3) {
   const request = require('request');
   request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`, function (err, res, body) {
     if (res.statusCode === 200) {
-      for (const char of JSON.parse(body).characters) {
-        request(char, function (err, res, body) {
-          if (res.statusCode === 200) {
-            console.log(JSON.parse(body).name);
-          } else {
-            console.log(err);
-          }
-        });
-      }
+      secondFunction(body);
     } else {
       console.log(err);
     }
