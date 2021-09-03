f322270b221391f227b1195001622358f87f092a
FatChicken277 <aramirez@truora.com>
2021-09-02 20:56:44 -0500

    remove conditional

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -47,7 +47,7 @@ int binary_recursive(int *array, int l, int r, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (!array || value > array[size - 1])
+	if (!array || size == 0)
 		return (-1);
 
 	return (binary_recursive(array, 0, size - 1, value));
