416414b3099c0ec8fbd40a92441df4ea67905c27
FatChicken277 <aramirez@truora.com>
2021-09-08 09:12:08 -0500

    use recursion in count_words

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,39 +5,11 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """This method returns a list containing the titles of all
-        hot articles for a given subreddit"""
-    headers = {"user-agent": "1637-holberton"}
-
-    if after is None:
-        return hot_list
-
-    if after == "":
-        r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
-            subreddit), headers=headers, allow_redirects=False)
-    else:
-        r = requests.get(
-            'https://www.reddit.com/r/{}/hot.json?after={}'.format(
-                subreddit, after), headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
-        return None
-
-    after = r.json().get("data").get("after")
-
-    for child in r.json().get("data").get("children"):
-        hot_list.append(child.get("data").get("title"))
-
-    return recurse(subreddit, hot_list, after)
-
-
-def count_words(subreddit, word_list):
+def process_subreddit(hot_subreddits, word_list):
     """This method parses the title of all hot articles,
         and prints a sorted count of given keywords."""
     list_all = {}
 
-    hot_subreddits = recurse(subreddit)
     if hot_subreddits is None:
         return
 
@@ -55,3 +27,30 @@ def count_words(subreddit, word_list):
     result = sorted(list_all.items(), key=lambda x: x[1], reverse=True)
     for k, v in result:
         print("{}: {}".format(k, v))
+
+
+def count_words(subreddit, word_list, hot_list=[], after=""):
+    """This method returns a list containing the titles of all
+        hot articles for a given subreddit"""
+    headers = {"user-agent": "1637-holberton"}
+
+    if after is None:
+        return process_subreddit(hot_list, word_list)
+
+    if after == "":
+        r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
+            subreddit), headers=headers, allow_redirects=False)
+    else:
+        r = requests.get(
+            'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
+    after = r.json().get("data").get("after")
+
+    for child in r.json().get("data").get("children"):
+        hot_list.append(child.get("data").get("title"))
+
+    return count_words(subreddit, word_list, hot_list, after)
