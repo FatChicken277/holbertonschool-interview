3fb6641c7ffad9cb82a5bb43ccffc8ca60561682
FatChicken277 <aramirez@truora.com>
2021-12-22 00:52:47 -0500

    add base files

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,20 @@
+#include "substring.h"
+
+/**
+ * find_substring - finds all the possible substrings
+ *
+ * @s: is the string to scan
+ * @words: array of words all substrings
+ * @nb_words: number of elements in the array words
+ * @n: holds the address
+ * Return: allocated array
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	int *value = 0, *value2 = 0;
+
+	if (*s && *words && nb_words && *n)
+		return (value2);
+
+	return (value);
+}
