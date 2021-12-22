63446e80031e6de796561c47780e817cdf3aa755
FatChicken277 <aramirez@truora.com>
2021-12-22 00:34:37 -0500

    refactor

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,69 +2,35 @@
 #include "lists.h"
 
 /**
- * copy_list - copy a linked list.
- * @head: pointer to the head node of the list to copy
- * @copy: pointer to the copy node where copying
- */
-void copy_list(listint_t *head, listint_t **copy)
-{
-	while (head != NULL)
-	{
-		add_nodeint_end(copy, head->n);
-		head = head->next;
-	}
-}
-
-/**
- * reverseList - reverse a linked list.
- * @head: pointer to the head node of the list to reverse
- */
-void reverseList(listint_t **head)
-{
-	listint_t *prevNode, *curNode;
-
-	if (*head != NULL)
-	{
-		prevNode = *head;
-		curNode = (*head)->next;
-		*head = (*head)->next;
-
-		prevNode->next = NULL;
-
-		while (*head != NULL)
-		{
-			*head = (*head)->next;
-			curNode->next = prevNode;
-
-			prevNode = curNode;
-			curNode = *head;
-		}
-
-		*head = prevNode;
-	}
-}
-
-/**
  * is_palindrome - verify if a linked list is a palindrome
  * @head: pointer to the head node of the list to reverse
  * Return: 1 if the list is palindrome 0 otherwise.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *reversed = NULL, *aux = *head;
+	int len = 0, i = 0, aux_array[10000];
+	listint_t *aux_head = *head;
 
-	copy_list(*head, &reversed);
-	*head = aux;
+	if (head == NULL || *head == NULL)
+		return (1);
 
-	reverseList(&reversed);
-
-	while (*head != NULL)
+	for (len = 0; aux_head != NULL; len++)
 	{
-		if (reversed->n != (*head)->n)
+		aux_array[len] = aux_head->n;
+		aux_head = aux_head->next;
+	}
+
+	if (len == 1)
+		return (1);
+
+	aux_head = *head;
+
+	for (i = 0; aux_head != NULL; i++, len--)
+	{
+		if (aux_head->n != aux_array[len - 1])
 			return (0);
 
-		reversed = reversed->next;
-		*head = (*head)->next;
+		aux_head = aux_head->next;
 	}
 
 	return (1);
